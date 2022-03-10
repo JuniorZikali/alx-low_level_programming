2d980835eff2056327e13dd74fec5870ef384e58
JuniorZikali <tadiwazikali@gmail.com>
2022-03-10 13:30:57 -0800

    Hello using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main prints the phrase "Programming is like bulding a multilingual puzzle
+ * Return: 0 is exited properly, 0 otherwise
+ */
+
+int main(void)
+{ 
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
