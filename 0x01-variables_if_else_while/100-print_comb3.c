4c619e7efb3ea91d38c04a749de9d016d548b34e
JuniorZikali <tadiwazikali@gmail.com>
2022-03-13 08:49:20 -0700

    combo 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combination of
+ * two digits followed by a new line
+ * Return: 0 Always (Sucess)
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
